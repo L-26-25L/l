8ae17788803e51bfee36d21bd581a125e458b43c
L-26-25L <Le_ena7@hotmail.com>
2026-01-18 03:48:03 +0300

    Add coursesData with assessment details

---
+++ data/courses.js
@@ -0,0 +1,49 @@
+export const coursesData = {
+  Economy: [
+    { type: "Midterm 1", total: 15, obtained: 13 },
+    { type: "Midterm 2", total: 15, obtained: 15 },
+    { type: "Quiz 1", total: 5, obtained: 5 },
+    { type: "Quiz 2", total: 5, obtained: 4 },
+    { type: "Quiz 3", total: 5, obtained: 5 },
+    { type: "Quiz 4", total: 5, obtained: 3 },
+    { type: "Quiz 5", total: 5, obtained: 4.5 },
+    { type: "Final Exam", total: 50, obtained: 0 }
+  ],
+
+  Arabica: [
+    { type: "Midterm", total: 20, obtained: 19 },
+    { type: "Various Activities", total: 20, obtained: 20 },
+    { type: "Final", total: 60, obtained: 0 }
+  ],
+
+  Islamic: [
+    { type: "Midterm", total: 20, obtained: 17 },
+    { type: "Performance Tasks", total: 20, obtained: 20 },
+    { type: "Final", total: 60, obtained: 0 }
+  ],
+
+  Administration: [
+    { type: "Midterm 1", total: 20, obtained: 20 },
+    { type: "Midterm 2", total: 20, obtained: 17 },
+    { type: "Report", total: 10, obtained: 10 },
+    { type: "Final", total: 50, obtained: 0 }
+  ],
+
+  Technology: [
+    { type: "Midterm 1", total: 20, obtained: 20 },
+    { type: "Midterm 2", total: 20, obtained: 18 },
+    { type: "Quiz 1", total: 5, obtained: 5 },
+    { type: "Quiz 2", total: 5, obtained: 3 },
+    { type: "Quiz 3", total: 5, obtained: 4 },
+    { type: "Final", total: 50, obtained: 0 }
+  ],
+
+  Math: [
+    { type: "Midterm", total: 25, obtained: 22 },
+    { type: "Quiz 1", total: 10, obtained: 10 },
+    { type: "Quiz 2", total: 10, obtained: 9 },
+    { type: "Quiz 3", total: 10, obtained: 9.5 },
+    { type: "Laboratory Test", total: 5, obtained: 5 },
+    { type: "Final", total: 50, obtained: 0 }
+  ]
+};
