022c31ebad6c345cb77c82751e03604baade1865
L-26-25L <Le_ena7@hotmail.com>
2026-02-01 23:57:55 +0300

    Add CourseBarChart component for grades distribution

---
+++ components/CourseBarChart.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
+
+export default function CourseBarChart({ data }) {
+  if (!data) return null;
+
+  const chartData = data.map((item) => ({
+    name: item.type,
+    obtained: item.obtained,
+    total: item.total
+  }));
+
+  return (
+    <div style={{ width: "100%", height: 300 }}>
+      <h3 style={{ marginBottom: 10 }}>Grades Distribution</h3>
+
+      <ResponsiveContainer>
+        <BarChart data={chartData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="obtained" fill="#734073" radius={[6, 6, 0, 0]} />
+          <Bar dataKey="total" fill="#d1c4d8" radius={[6, 6, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
