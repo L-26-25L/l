8583b2b21a83374ad72efc5ee02156d7c5934b65
L-26-25L <Le_ena7@hotmail.com>
2026-02-07 04:41:39 +0300

    Update CoursePie.jsx

--- components/CoursePie.jsx
+++ components/CoursePie.jsx
@@ -1,17 +1,29 @@
-import { PieChart, Pie, Cell } from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 export default function CoursePie({ obtained, total }) {
   const data = [
-    { name:"Mine", value: obtained },
-    { name:"Remaining", value: total - obtained }
+    { name: "Obtained", value: obtained },
+    { name: "Remaining", value: total - obtained }
   ];
 
   return (
-    <PieChart width={220} height={220}>
-      <Pie data={data} dataKey="value" outerRadius={80}>
-        <Cell fill="#5baee5" />
-        <Cell fill="#e6e6ef" />
-      </Pie>
-    </PieChart>
+    <div style={{ width: "100%", height: 180 }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie 
+            data={data} 
+            dataKey="value" 
+            innerRadius={0} 
+            outerRadius="80%" 
+            startAngle={90} 
+            endAngle={450}
+            stroke="none"
+          >
+            <Cell fill="#1a3a5a" /> {/* أزرق غامق */}
+            <Cell fill="#9dc6e0" /> {/* أزرق فاتح */}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
